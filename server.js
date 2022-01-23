fb3c887462943e67438623f05324bce2572e0be8
max-muoto <41130755+max-muoto@users.noreply.github.com>
2022-01-23 14:03:45 -0500

    Linting Change

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ fs.readFile('./www/index.html', (err, data) => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/html');
         res.end(data);
-      })
+    })
 
-      server.listen(port, () => {
+    server.listen(port, () => {
         console.log(`Server listening on port ${port}`);
     })
-  })
+})
